e2aba9629fdc207da4818b5a907b9288231351ac
dazcodes <darabojuwoye@hotmail.co.uk>
2024-10-02 21:32:20 +0100

    Successful attempt of getting the AlgoTest to pass

--- algo-exercise/getting-started/src/test/java/codingblackfemales/gettingstarted/MyAlgoTest.java
+++ algo-exercise/getting-started/src/test/java/codingblackfemales/gettingstarted/MyAlgoTest.java
@@ -40,39 +40,17 @@ public class MyAlgoTest extends AbstractAlgoTest {
     private final MessageHeaderEncoder headerEncoder = new MessageHeaderEncoder();
     private final BookUpdateEncoder encoder = new BookUpdateEncoder();
 
-    private AlgoContainer container;
 
     @Override
     public AlgoLogic createAlgoLogic() {
         //this adds your algo logic to the container classes
-        final TestNetwork network = new TestNetwork();
-        final Sequencer sequencer = new DefaultSequencer(network);
 
-        final RunTrigger runTrigger = new RunTrigger();
-        final Actioner actioner = new Actioner(sequencer);
-
-        final MarketDataChannel marketDataChannel = new MarketDataChannel(sequencer);
-        final OrderChannel orderChannel = new OrderChannel(sequencer);
-        final OrderBook book = new OrderBook(marketDataChannel, orderChannel);
-
-        final OrderBookInboundOrderConsumer orderConsumer = new OrderBookInboundOrderConsumer(book);
-
-        container = new AlgoContainer(new MarketDataService(runTrigger), new OrderService(runTrigger), runTrigger, actioner);
-        //set my algo logic
-        container.setLogic(new MyAlgoLogic());
-
-        network.addConsumer(new LoggingConsumer());
-        network.addConsumer(book);
-        network.addConsumer(container.getMarketDataService());
-        network.addConsumer(container.getOrderService());
-        network.addConsumer(orderConsumer);
-        network.addConsumer(container);
 
         return new MyAlgoLogic();
     }
 
 
-    private UnsafeBuffer createSampleMarketDataTick(){
+    private UnsafeBuffer createTick(long minBidPrice, long maxAskPrice){
         final ByteBuffer byteBuffer = ByteBuffer.allocateDirect(1024);
         final UnsafeBuffer directBuffer = new UnsafeBuffer(byteBuffer);
 
@@ -84,78 +62,55 @@ public class MyAlgoTest extends AbstractAlgoTest {
         encoder.instrumentId(123L);
         encoder.source(Source.STREAM);
 
+        //different ranges for minBidPrice
         encoder.bidBookCount(3)
-                .next().price(98L).size(100L)
-                .next().price(95L).size(200L)
-                .next().price(91L).size(300L);
+                .next().price(minBidPrice).size(100L)
+                .next().price(minBidPrice - 1).size(200L)
+                .next().price(minBidPrice - 2).size(300L);
 
-        encoder.askBookCount(4)
-                .next().price(100L).size(101L)
-                .next().price(110L).size(200L)
-                .next().price(115L).size(5000L)
-                .next().price(119L).size(5600L);
+//different ranges for minAskPrice
+        encoder.askBookCount(3)
+                .next().price(maxAskPrice).size(150L)
+                .next().price(maxAskPrice + 1).size(250L)
+                .next().price(maxAskPrice + 2).size(350L);
 
+
+//set instrument status
         encoder.instrumentStatus(InstrumentStatus.CONTINUOUS);
-
         return directBuffer;
     }
 
-    private UnsafeBuffer createSampleMarketDataTick2(){
-        final ByteBuffer byteBuffer = ByteBuffer.allocateDirect(1024);
-        final UnsafeBuffer directBuffer = new UnsafeBuffer(byteBuffer);
-
-        //write the encoded output to the direct buffer
-        encoder.wrapAndApplyHeader(directBuffer, 0, headerEncoder);
-
-        //set the fields to desired values
-        encoder.venue(Venue.XLON);
-        encoder.instrumentId(123L);
-        encoder.source(Source.STREAM);
-
-        encoder.bidBookCount(3)
-                .next().price(95L).size(100L)
-                .next().price(93L).size(200L)
-                .next().price(91L).size(300L);
-
-        encoder.askBookCount(4)
-                .next().price(98L).size(501L)
-                .next().price(101L).size(200L)
-                .next().price(110L).size(5000L)
-                .next().price(119L).size(5600L);
-
-        encoder.instrumentStatus(InstrumentStatus.CONTINUOUS);
-
-        return directBuffer;
-    }
 
     @Test
 
     public void testDispatchThroughSequencer() throws Exception {
         //create a sample market data tick....
-        send(createTick());
+//        send(createTick());
 
-//        // Ensure that the state is initialized properly
-//        assertEquals(0, container.getState().getChildOrders().size());
-//
-//        // Tick 1: Basic market data update
-//        send(createSampleMarketDataTick(98L, 100L));
-//        assertEquals(1, container.getState().getChildOrders().size());
-//
-//        // Tick 2: Increased bid prices
-//        send(createSampleMarketDataTick(99L, 101L));
-//        assertEquals(2, container.getState().getChildOrders().size());
-//
-//        // Tick 3: Decreased ask prices
-//        send(createSampleMarketDataTick(97L, 99L));
-//        assertEquals(3, container.getState().getChildOrders().size());
-//
-//        // Tick 4: Prices stabilize
-//        send(createSampleMarketDataTick(98L, 100L));
-//        assertEquals(3, container.getState().getChildOrders().size());
-//
-//        // Tick 5: Significant price change
-//        send(createSampleMarketDataTick(101L, 103L));
-//        assertEquals(3, container.getState().getChildOrders().size());
+
+        // Tick 1: Basic market data update
+        send(createTick(98L, 100L));
+        assertEquals(3, container.getState().getChildOrders().size());
+
+        // Tick 2: An increase in the bid prices
+        send(createTick(99L, 101L));
+        assertEquals(3, container.getState().getChildOrders().size());
+
+        // Tick 3: A decrease in the ask prices
+        send(createTick(97L, 99L));
+        assertEquals(3, container.getState().getChildOrders().size());
+
+        // Tick 4: The prices are stable
+        send(createTick(98L, 100L));
+        assertEquals(3, container.getState().getChildOrders().size());
+
+        // Tick 5: Significant price change
+        send(createTick(101L, 103L));
+        assertEquals(3, container.getState().getChildOrders().size());
+
+        //simple assert to check we had 3 orders created
+        assertEquals(container.getState().getChildOrders().size(), 3);
+
     }
 
 
